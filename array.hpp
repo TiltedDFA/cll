20bab088f58e44f33478a04728a66d01eacf1011
MalikT <maliktremain1@gmail.com>
2025-01-24 06:27:33 +0000

    fixed array issue

--- array.hpp
+++ array.hpp
@@ -19,13 +19,18 @@ namespace cll
 			cll::Fill(data_, data_ + SIZE, value);
 		}
 		
+		[[nodiscard]]
 		constexpr cll::sizet Size() const noexcept {return SIZE;}
 
-		constexpr T const* cbegin() const noexcept {return data_;}
+		// constexpr T const* cbegin() const noexcept {return data_;}
 		constexpr T* begin() noexcept {return data_;}
+		//for some reason when trying to use this array as a const in a for each
+		//loop, it doesn't look for cbegin but rather fails as it wants a begin marked as const.
+		constexpr T const* begin() const noexcept {return data_;}
 
-		constexpr T const* cend() const noexcept {return data_ + SIZE;}
+		// constexpr T const* cend() const noexcept {return data_ + SIZE;}
 		constexpr T* end() noexcept {return data_ + SIZE;}
+		constexpr T const* end() const noexcept {return data_ + SIZE;}
 
 		constexpr T& operator[](cll::sizet idx) noexcept {return data_[idx];}
 		constexpr T const& operator[](cll::sizet idx) const noexcept {return data_[idx];}
